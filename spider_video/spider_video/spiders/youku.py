f48bafcf579703d651e329299f5b9e369c46448b
snowdream1314 <595968602@qq.com>
2016-02-29 16:16:23 +0800

    update spider_video

--- spider_video/spider_video/spiders/youku.py
+++ spider_video/spider_video/spiders/youku.py
@@ -44,28 +44,31 @@ class youku_video_spider(Spider):
             category_name = source['c_name']
             category_href = source['c_href']
             print category_name
-            if category_id == 8:
-                self.parseCo13(category_id, category_name, category_href)
+            if category_id == 10:
+                self.parseCol4(category_id, category_name, category_href)
             #分为两类不同结构
-#             if category_id in [2, 4, 6, 8, 12, 14]:
-#                 if category_id == 2: continue
-#                 if category_id == 4: continue
-#                 self.parseCo13(category_id, category_name, category_href)
+            if category_id in [2, 4, 6, 8, 12, 14]:
+                if category_id == 2: continue
+                if category_id == 4: continue
+                if category_id == 6: continue
+                if category_id == 8: continue
+                self.parseCol3(category_id, category_name, category_href)
 #             else:
-#                 self.parseCo14(category_id, category_name, category_href)
+#                 self.parseCol4(category_id, category_name, category_href)
                 
                 
-    def parseCo13(self, category_id, category_name, category_href):
-        print "parseCo13"
+    def parseCol3(self, category_id, category_name, category_href):
+        print "parseCol3"
         
         source_url = category_href
+#         source_url = "http://www.youku.com/v_olist/c_100_g__a__sg__mt__lg__q__s_1_r_0_u_0_pt_0_av_0_ag_0_sg__pr__h__d_1_p_13.html"
         while 1:
-            
+             
             print source_url
-            
+             
             selector = load_content(source_url, method='GET')
             if selector is None: return
-            
+             
             lists = selector.findAll("div", {"class":"p p-small"})
             for list in lists:
                 print ''
@@ -73,7 +76,7 @@ class youku_video_spider(Spider):
                 print ''
                 #图片链接
 #                 img_link = list.find("div", {"class":"p-thumb"}).find("img").attrs['src']
-                
+                 
                 #主角
 #                 if list.find("span", {"class":"p-actor"}):
 #                     actor_def = list.find("span", {"class":"p-actor"})
@@ -83,20 +86,20 @@ class youku_video_spider(Spider):
 #                     actor = ''
 #                     actor_link = ''
 #                 print actor
-                 
+                  
                 #资源标示
 #                 item_id = item_link.split('_')[-1].split('.')[0]
 #                 print "Grab item %s:" % item_id
-
+ 
                 #更新情况
 #                 update_status = list.find("div", {"class":"p-thumb-taglb"}).find("span", {"class":"p-status"}).get_text().strip()
-
+ 
                 #特别标签，如“会员免费”
                 if list.find("div", {"class":"p-thumb-tagrt"}) is not None and list.find("div", {"class":"p-thumb-tagrt"}).find("span", {"class":"sub-txt"}) is not None:
                     authority = list.find("div", {"class":"p-thumb-tagrt"}).find("span", {"class":"sub-txt"}).get_text()
                 else:
                     authority = "free"
-                    
+                     
                 #资源标题
                 print "---------------------------------"
                 video_name = list.find("div", {"class":"p-meta-title"}).find("a").attrs['title']
@@ -105,18 +108,18 @@ class youku_video_spider(Spider):
                 print ''   
                 #评分
                 video_rating = list.find("div", {"class":"p-thumb-tagrb"}).find("span", {"class":"p-rating"}).get_text().strip()
-                
+                 
                 #资源链接
                 video_link = list.find("div", {"class":"p-link"}).find("a").attrs['href']
                 video_id = video_link.split("id_z")[-1].split(".")[0]
-                
-                item_selector = load_content(video_link, method='GET')
+                 
+                item_selector = load_content(video_link, method='GET', time_sleep=True, referer=category_href)
                 if item_selector is None: return
-                
+                 
                 item_showInfo = item_selector.find("div", {"class":"showInfo poster_w yk-interact"}).find("ul", {"class":"baseinfo"})
                 item_basedata = item_selector.find("div", {"class":"showInfo poster_w yk-interact"}).find("ul", {"class":"basedata"})
                 item_basenotice = item_selector.find("div", {"class":"showInfo poster_w yk-interact"}).find("div", {"class":"basenotice"})
-                
+                 
                 #播放链接,视频id
                 show_link = item_showInfo.find("li", {"class":"link"}).find("a")
                 if show_link is not None:
@@ -125,7 +128,7 @@ class youku_video_spider(Spider):
                     show_link = item_selector.find("ul", {"class":"baseaction"}).find("li", {"class":"action"}).find("a").attrs['href']
 #                 video_id = show_link.split('from=')[-1].split('.')[-1]
                 print "video_id is : %s" % video_id
-                
+                 
                 #豆瓣评分
                 score_db = item_selector.find("div", {"class":"showInfo poster_w yk-interact"}).find("div", {"class":"score_db score_db_btn"})
                 if score_db is not None:
@@ -134,11 +137,11 @@ class youku_video_spider(Spider):
                     score_db = ''
                 #图片链接
                 img_link = item_showInfo.find("li", {"class":"thumb"}).find("img").attrs['src']
-                
+                 
                 #视频画质
 #                 video_quality = item_showInfo.find("li", {"class":"ishd"}).find("span").attrs['class'].split('__')[-1]
 #                 print "quality is : %s" % video_quality
-                
+                 
                 #视频别名
                 video_alias = item_showInfo.find("span", {"class":"alias"})
                 if video_alias is not None:
@@ -146,7 +149,7 @@ class youku_video_spider(Spider):
                 else:
                     video_alias = ''
                 print "video_alias is : %s" % video_alias
-                
+                 
                 #视频时长
                 video_duration = item_showInfo.find("span", {"class":"duration"}) 
                 if video_duration is not None:
@@ -154,10 +157,10 @@ class youku_video_spider(Spider):
                 else:
                     video_duration = ''
                 print "video_duration is : %s" % video_duration
-                
+                 
                 #上映时间和优酷上映时间
                 time_items = item_showInfo.findAll("span", {"class":"pub"}) 
-                if len(time_items) == 1:                                                                                               
+                if len(time_items) == 1 :                                                                                               
                     show_time = time_items[0].get_text().split(":")[-1].replace("\n", "").replace("\t", "").replace("\r", "").strip()
                     yk_showtime = ''
                 elif len(time_items) == 2:   
@@ -168,12 +171,20 @@ class youku_video_spider(Spider):
                     yk_showtime = ''
                 print "show_time is : %s" % show_time
                 print "yk_showtime is : %s" % yk_showtime
-                
+                 
                 #地区和类型
-                video_area = item_showInfo.find("span", {"class":"area"}).find("a").get_text()
-                video_type = item_showInfo.find("span", {"class":"type"}).attrs['title']
+                video_area = item_showInfo.find("span", {"class":"area"}).find("a")
+                if video_area is not None:
+                    video_area = video_area.get_text()
+                else:
+                    video_area = ''
+                video_type = item_showInfo.find("span", {"class":"type"})
+                if video_type is not None and video_type.find("a") is not None:
+                    video_type = video_type.attrs['title']
+                else:
+                    video_type = ''
                 print video_area, video_type
-                
+                 
                 #导演和主演
                 video_director = item_showInfo.find("span", {"class":"director"})
                 if video_director is None:
@@ -185,6 +196,15 @@ class youku_video_spider(Spider):
 #                         video_director = list.find("span", {"class":"p-actor"}).find("a").get_text()
 #                     else:
                     video_director = ''
+                     
+                if category_id == 12:   #教育类标签不一样
+                    video_director = video_area
+                    video_area = ''
+                if category_id == 14:
+                    video_director = item_showInfo.find("li", {"class":"row1"})
+                    if video_director is not None and video_director.find("a") is not None:
+                        video_director = video_director.find("a").get_text()
+                     
                 video_actors = item_showInfo.find("span", {"class":"actor"})
                 if video_actors is not None and video_actors.find("a") is not None:
                     video_actors = video_actors.attrs['title']
@@ -192,7 +212,7 @@ class youku_video_spider(Spider):
                     video_actors = ''
                 print "director is : %s" % video_director
                 print "actors : %s" % video_actors
-                
+                 
                 #动漫的适用年龄
                 video_agefor = ''
                 if category_id == 8:
@@ -203,14 +223,14 @@ class youku_video_spider(Spider):
                         if video_actors_item.find("span", {"class":"type"}) is not None and video_actors_item.find("span", {"class":"type"}).find("a") is not None:
                             video_actors = video_actors_item.find("span").attrs['title']
                             print "actors : %s" % video_actors
-                    
+                     
                 #播出频道
                 video_TV = item_showInfo.find("span", {"class":"broadcast"})
                 if video_TV is not None:
                     video_TV = video_TV.find("a").get_text()
                 else:
                     video_TV = ''
-                    
+                     
                 #总播放量、评论数量、'顶'数量
                 played_count = item_basedata.find("span", {"class":"play"}).get_text().split(":")[-1].strip()
                 comment_num = item_basedata.find("span", {"class":"comment"}).find("em", {"class":"num"}).get_text().strip()
@@ -218,7 +238,7 @@ class youku_video_spider(Spider):
                 print "played_count is : %s" % played_count
                 print "comment_num is : %s" % comment_num
                 print "support_num is : %s" % support_num
-                
+                 
                 #更新状态
                 update_schedule = item_basenotice.get_text().replace("\n", "").replace("\t", "").replace("\r", "").strip()
                 if len(update_schedule) > 10:
@@ -226,7 +246,7 @@ class youku_video_spider(Spider):
                 else:
                     update_status = 1
                 print "update_schedule is : %s " % update_schedule
-                
+                 
                 #视频简介
                 item_desc = item_selector.find("div", {"class":"box nBox"}).find("div", {"class":"detail"})
                 if item_desc is not None:
@@ -239,8 +259,8 @@ class youku_video_spider(Spider):
                 else:
                     video_desc = ''
                 print "video_desc is: %s" % (video_desc[:40])
-                   
-                
+                    
+                 
 #                 videos = db_video.query("select * from cv_video_album where va_video_id=%s", item_id)
                 videos = db_video.query("select * from cv_video_detail_msg where cv_video_id=%s", video_id)
                 if len(videos) == 0:
@@ -250,6 +270,96 @@ class youku_video_spider(Spider):
                                     cv_video_actors=video_actors, cv_comment_num=comment_num, cv_support_num=support_num, cv_update_schedule=update_schedule, cv_video_img=img_link, cv_video_auth=authority,
                                     cv_show_link=show_link, cv_douban_rate=score_db, cv_update_status=update_status, cv_video_duration=video_duration, cv_video_TV=video_TV, cv_video_agefor=video_agefor,
                                     cv_show_time=show_time, cv_yk_showtime=yk_showtime)
+                    print "insert successfully"
+                else:
+                    print "video already exists"
+                    continue
+                 
+            db_video.commit()
+             
+            next_page = selector.find("div", {"class":"yk-pager"}).find("li", {"class":"next"}).find("a")
+            if next_page:
+                next_link = next_page.attrs['href']
+                source_url = "http://www.youku.com" + str(next_link)
+            else:
+                print "crawled over,category_id is : %s" % category_id
+                return
+                
+        
+        
+        pass
+    
+    
+    def parseCol4(self, category_id, category_name, category_href):
+        
+        print "parseCol4"
+        
+        source_url = category_href
+        print source_url
+        
+        while 1:
+            
+            selector = load_content(category_href, method='GET')
+            if selector is None: return
+            
+            lists = selector.findAll("div", {"class":"yk-col4"})
+            for list in lists:
+                v_thumb = list.find("div", {"class":"v"}).find("div", {"class":"v-thumb"})
+                v_link = list.find("div", {"class":"v"}).find("div", {"class":"v-link"})
+                v_meta = list.find("div", {"class":"v"}).find("div", {"class":"v-meta va"})
+                
+                #图片链接
+                img_link = v_thumb.find("img").attrs['src']
+                
+                #视频质量
+                video_quality = v_thumb.find("div", {"class":"v-thumb-tagrt"}).find("i")
+                if video_quality is not None:
+                    video_quality = video_quality.attrs['title']
+                    print "video_quality is : %s" % video_quality
+                    
+                #视频时长
+                video_duration = v_thumb.find("div", {"class":"v-thumb-tagrb"}).find("span", {"class":"v-time"})
+                if video_duration is not None:
+                    video_duration = video_duration.get_text()
+                    print "video_duration is : %s" %video_duration
+                    
+                #视频演员作者
+                video_actors = v_thumb.find("div", {"class":"v-thumb-taglb"})
+                if video_actors is not None:
+                    video_actors = video_actors.find("span", {"class":"v-status"}).get_text().replace("\n", "").replace("\r", "").replace("\t", "").strip()
+                elif v_meta.find("a", {"class":"v-username"}) is not None:
+                    video_actors = v_meta.find("a", {"class":"v-username"}).get_text().strip()
+                else:
+                    video_actors = ''
+                print "video_actors is : %s" % video_actors
+                
+                #视频播放链接
+                show_link = v_link.find("a").attrs['href']
+                video_id = show_link.split("id_")[-1].split(".html")[0]
+                print "video_id is : %s" %video_id
+                
+                #视频标题
+                video_name = v_meta.find("div", {"class":"v-meta-title"}).find("a").get_text()
+                print "video_name is : %s" % video_name
+                
+                item_selector = load_content(show_link, method='GET', time_sleep=True)
+                if item_selector is None: return
+                
+                #视频支持数
+                support_num = item_selector.find("div", {"class":"fn-updown"}).find("div", {"class":"fn-up"}).find("span", {"class":"num"}).get_text().strip()
+                print "support_num is : %s" % support_num
+                
+                #视频评论数和播放总数
+                comment_num = item_selector.find("div", {"class":"fn-wrap"}).find("a", {"id":"video_comment_number"}).attrs['title']
+                played_count = item_selector.find("div", {"class":"fn-wrap"}).find("span", {"id":"videoTotalPV"}).get_text().replace(u"播放", "").strip()
+                print "comment_num is : %s" % comment_num
+                print "played_count is : %s" % played_count
+                
+                videos = db_video.query("select * from cv_video_detail_msg where cv_video_id=%s", video_id)
+                if len(videos) == 0:  
+                    db_video.insert("cv_video_detail_msg", cv_category_id=category_id, cv_video_name=video_name, cv_video_id=video_id, cv_played_count=played_count, 
+                                    cv_video_actors=video_actors, cv_comment_num=comment_num, cv_support_num=support_num, cv_video_img=img_link, cv_video_auth="free",
+                                    cv_show_link=show_link, cv_video_duration=video_duration)
                     print "insert successfully"
                 else:
                     print "video already exists"
@@ -265,14 +375,5 @@ class youku_video_spider(Spider):
                 print "crawled over,category_id is : %s" % category_id
                 return
                 
-        
-        
-        pass
-    
-    
-    def parseCo14(self, category_id, category_name, category_href):
-        
-        selector = load_content(category_href, method='GET')
-        if selector is None: return
         pass
                 
--- spider_video/spider_video/utils/load_content.py
+++ spider_video/spider_video/utils/load_content.py
@@ -6,11 +6,14 @@ from bs4 import BeautifulSoup
 import httplib2
 
 
-def load_content(url, time_sleep=False, cookie='', method='GET', headers=None):
+def load_content(url, time_sleep=False, cookie='', method='GET', headers=None, referer=None, host=None):
     print "load_content"
     
     if time_sleep:
         time.sleep(random.randint(1,3))
+    
+    if host is None:
+        host = "www.youku.com"
         
     if headers is None:
         user_agent_list = [\
@@ -41,11 +44,11 @@ def load_content(url, time_sleep=False, cookie='', method='GET', headers=None):
         ]
 #         useragent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36"
         useragent = random.choice(user_agent_list)
-        headers = {"User-Agent":useragent, "Upgrade-Insecure-Requests":"1", "Cookie":cookie, "Host":"www.youku.com"}
+        headers = {"User-Agent":useragent, "Upgrade-Insecure-Requests":"1", "Cookie":cookie, "Host":host}
         
     try:
         h = httplib2.Http()
-        response, content = h.request(url, method='GET', headers=headers)
+        response, content = h.request(url, method='GET', headers=headers, redirections=10)
         print str(response.status)
         if response.status == 200:
             return BeautifulSoup(content)
